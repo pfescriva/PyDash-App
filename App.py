2fe19d199d85633e93fc52bee2b7d4c2696c3941
Pere Fuster Escrivà <71879463+pfescriva@users.noreply.github.com>
2021-03-27 11:33:20 +0100

    Load packages

---
+++ App.py
@@ -0,0 +1,20 @@
+
+import dash
+from dash.dependencies import Input, Output, State
+
+import dash_html_components as html
+import dash_core_components as dcc
+import dash_table as dt
+import pandas as pd
+import plotly.express as px
+import json
+import numpy as np
+import plotly.graph_objects as go
+import sklearn as sk
+
+
+
+
+
+
+
