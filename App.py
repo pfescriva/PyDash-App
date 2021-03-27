0db77d698b200f57ce95d5354d3e221b8b5d420e
Pere Fuster Escrivà <71879463+pfescriva@users.noreply.github.com>
2021-03-27 12:47:13 +0100

    App first stage

--- App.py
+++ App.py
@@ -12,6 +12,27 @@ import numpy as np
 import plotly.graph_objects as go
 import sklearn as sk
 
+app = dash.Dash(__name__, title = "My Dashboard")
+
+# Loading dataset "House Sales":
+sales = pd.read_csv('raw_sales.csv')
+
+# Data Manipulation here: 
+
+# PyDash is in a way similar to RShinny. Instead of the UI, you have to append to the App.layout 
+# app.layout: the layout part of the app, and you create there the .html components 
+
+
+import dash
+import dash_html_components as html
+
+app = dash.Dash(__name__, title="2021 (Mis cojones) Dash Python App")
+# 
+app.layout = html.H1(app.title) 
+# 
+if __name__ == '__main__':
+# app.server.run() 
+
 
 
 
